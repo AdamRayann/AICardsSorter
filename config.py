ebcf6bdc77e006f0b6438920381683a2b162df78
AdamRayann <129179113+AdamRayann@users.noreply.github.com>
2025-02-22 12:45:46 +0200

    code updated

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
 
-# Detect if running inside Docker
-RUNNING_IN_DOCKER = os.getenv("RUNNING_IN_DOCKER", "false").lower() == "true"
+BASE_URL = os.getenv("BASE_URL", "http://localhost:3000")  # Use Docker service name
 
-# Use `host.docker.internal` inside Docker, otherwise use localhost
-WEKAN_HOST = "host.docker.internal" if RUNNING_IN_DOCKER else "127.0.0.1"
-BASE_URL = f"http://{WEKAN_HOST}:3000"
 USERNAME = "adminn"
 PASSWORD = "adminn"
 GROQ_API_KEY = "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"
